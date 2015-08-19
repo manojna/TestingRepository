db5f65b534e5b1259cf305af3b9c8e0210ef3399
manojna <manojna.bikkina@paradigmcreatives.com>
2015-08-19 18:36:30 +0530

    Initial Commit

---
+++ SampleProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleProject
+//
+//  Created by Manogna on 8/19/15.
+//  Copyright (c) 2015 paradigmcreatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleProject
+//
+//  Created by Manogna on 8/19/15.
+//  Copyright (c) 2015 paradigmcreatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
